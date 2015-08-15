4186b7f7d8f141046d973775cb05ae672512b6f0
Tim Brandin <tim.brandin@gmail.com>
2015-08-15 12:35:41 +0200

    Fixed an issue with multiple steps.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pipeline:templating',
-  version: '0.0.1',
+  version: '0.1.0',
   summary: 'Templating in the pipeline',
   git: 'https://github.com/MeteorPipeline/templating',
   documentation: null
@@ -10,9 +10,9 @@ Package.registerBuildPlugin({
   name: 'pipeline:html',
   use: [
     'pipeline:html@0.0.1',
-    'caching-html-compiler@1.0.1-plugins.0',
-    'ecmascript@0.1.3-plugins.5',
-    'templating-tools@0.0.2-plugins.0'
+    'caching-html-compiler@1.0.1-rc.0',
+    'ecmascript@0.1.3-rc.0',
+    'templating-tools@1.0.0-rc.0'
   ],
   sources: [
     'plugin/html_scanner.js',
@@ -28,7 +28,7 @@ Package.onUse(function (api) {
   api.addFiles('templating.js', 'client');
   api.export('Template', 'client');
 
-  api.use('underscore@1.0.4-plugins.0'); // only the subset in packages/blaze/microscore.js
+  api.use('underscore@1.0.4-rc.0'); // only the subset in packages/blaze/microscore.js
 
   api.use('isobuild:compiler-plugin@1.0.0');
 
@@ -36,11 +36,10 @@ Package.onUse(function (api) {
   // Blaze, so anybody using templating (eg apps) need to implicitly use
   // 'meteor' and 'blaze'.
   api.use([
-    'blaze@2.1.3-plugins.1',
-    'spacebars@1.0.7-plugins.1'
+    'blaze@2.1.3-rc.0',
+    'spacebars@1.0.7-rc.0'
   ]);
   api.imply(['meteor', 'blaze', 'spacebars'], 'client');
 
   api.addFiles(['dynamic.html', 'dynamic.js'], 'client');
-  api.export('TemplatePipelineCompiler');
 });
--- plugin/plugin.js
+++ plugin/plugin.js
@@ -1,6 +1,6 @@
 // The CompileResult type for this CachingCompiler is the return value of
 // htmlScanner.scan: a {js, head, body, bodyAttrs} object.
-TemplatePipelineCompiler.push(() => new CachingHtmlCompiler(
+TemplatePipelineCompiler.add('CachingHtmlCompiler', new CachingHtmlCompiler(
   "templating",
   TemplatingTools.scanHtmlForTags,
   TemplatingTools.compileTagsWithSpacebars
